d6dea02a4b116d3e683c9ca762f53bb0bb4b1ea1
merrill sequeira <emseq@hotmail.com>
2021-11-14 22:39:05 +0530

    added user input for N

--- random.py
+++ random.py
@@ -1,4 +1,4 @@
-n=99
+n=int(input('enter a number! '))
 
 while n>=1: 
     print('{} bottles of beeer on the wall. {} botles of beer. take one down pass it all around {} bottles of beer on the wall'.format(n,n,n-1))
